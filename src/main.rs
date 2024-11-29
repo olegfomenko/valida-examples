d80fa6f7ac93369170a42dfb18618a5afad529ac
olegfomenko <oleg.fomenko2002@gmail.com>
2024-11-30 00:37:42 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 #![no_main]
 
+use k256::elliptic_curve::group::{Curve, GroupEncoding};
 use k256::elliptic_curve::Group;
 use k256::Scalar;
-use std::ops::{Add, Mul};
-use k256::elliptic_curve::group::Curve;
 use rand::rngs::OsRng;
+use std::ops::{Add, Mul};
 
 valida_rs::entrypoint!(main);
 fn main() {
-
     let mut csprng = OsRng;
 
     let private_key = Scalar::generate_biased(&mut csprng);
@@ -16,12 +15,10 @@ fn main() {
     let g = k256::ProjectivePoint::random(&mut csprng);
     let h = k256::ProjectivePoint::random(&mut csprng);
 
-    let _ = g
-        .mul(private_key)
-        .add(h.mul(Scalar::from(amount)));
-    //
-    // valida_rs::io::println("Commitment: {:?}", commitment.to_affine());
-    // valida_rs::io::println("Private key: {:?}", private_key);
-    // valida_rs::io::println("G: {:?}", g.to_affine());
-    // valida_rs::io::println("H: {:?}", h.to_affine());
+    let commitment = g.mul(private_key).add(h.mul(Scalar::from(amount)));
+
+    valida_rs::io::println(&hex::encode(commitment.to_bytes()));
+    valida_rs::io::println(&hex::encode(private_key.to_bytes()));
+    valida_rs::io::println(&hex::encode(h.to_bytes()));
+    valida_rs::io::println(&hex::encode(h.to_bytes()));
 }
