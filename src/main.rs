be692d5f27c52cdd533e6991e721b92645b39f47
olegfomenko <oleg.fomenko2002@gmail.com>
2024-11-29 18:57:33 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,17 @@ use std::ops::{Add, Mul};
 
 valida_rs::entrypoint!(main);
 
+#[no_mangle]
 fn main() {
-    let private_key_str = valida_rs::io::read_and_deserialize::<String>().unwrap();
+    let private_key_str = valida_rs::io::read_line::<String>().unwrap();
     let private_key = serde_json::from_str::<Scalar>(&private_key_str).unwrap();
 
-    let amount = valida_rs::io::read_and_deserialize::<u64>().unwrap();
+    let amount = valida_rs::io::read_line::<u64>().unwrap();
 
-    let g_str = valida_rs::io::read_and_deserialize::<String>().unwrap();
+    let g_str = valida_rs::io::read_line::<String>().unwrap();
     let g = serde_json::from_str::<AffinePoint>(&g_str).unwrap();
 
-    let h_str = valida_rs::io::read_and_deserialize::<String>().unwrap();
+    let h_str = valida_rs::io::read_line::<String>().unwrap();
     let h =  serde_json::from_str::<AffinePoint>(&h_str).unwrap();
 
     let commitment = ProjectivePoint::from(g)
