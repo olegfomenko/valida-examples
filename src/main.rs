4369761db6d6d0a9997d93264d596d5fe8702386
olegfomenko <oleg.fomenko2002@gmail.com>
2024-11-29 20:03:00 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,12 @@ fn main() {
     let g = k256::ProjectivePoint::random(&mut csprng);
     let h = k256::ProjectivePoint::random(&mut csprng);
 
-    let commitment = g
+    let _ = g
         .mul(private_key)
         .add(h.mul(Scalar::from(amount)));
-
-    valida_rs::io::println("Commitment: {:?}", commitment.to_affine());
-    valida_rs::io::println("Private key: {:?}", private_key);
-    valida_rs::io::println("G: {:?}", g.to_affine());
-    valida_rs::io::println("H: {:?}", h.to_affine());
+    //
+    // valida_rs::io::println("Commitment: {:?}", commitment.to_affine());
+    // valida_rs::io::println("Private key: {:?}", private_key);
+    // valida_rs::io::println("G: {:?}", g.to_affine());
+    // valida_rs::io::println("H: {:?}", h.to_affine());
 }
