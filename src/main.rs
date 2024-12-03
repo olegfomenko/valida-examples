d681f643f8858db07c7f922b8eb7bf4bce5c0638
olegfomenko <oleg.fomenko2002@gmail.com>
2024-12-03 12:31:16 +0200

    upd

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,17 @@
 use k256::elliptic_curve::group::{Curve, GroupEncoding};
 use k256::elliptic_curve::Group;
 use k256::Scalar;
-use rand::rngs::OsRng;
 use std::ops::{Add, Mul};
 
 valida_rs::entrypoint!(main);
 fn main() {
-    let mut csprng = OsRng;
+    let private_key = Scalar::from(12345678909876554321u128);
+    let amount = Scalar::from(12345u64);
 
-    let private_key = Scalar::generate_biased(&mut csprng);
-    let amount = 12345u64;
-    let g = k256::ProjectivePoint::random(&mut csprng);
-    let h = k256::ProjectivePoint::random(&mut csprng);
+    let g = k256::ProjectivePoint::GENERATOR;
+    let h = g.mul(k256::Scalar::from(12345));
 
-    let commitment = g.mul(private_key).add(h.mul(Scalar::from(amount)));
+    let commitment = g.mul(private_key).add(h.mul(amount));
 
     valida_rs::io::println(&hex::encode(commitment.to_bytes()));
     valida_rs::io::println(&hex::encode(private_key.to_bytes()));
